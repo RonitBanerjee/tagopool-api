a62f4165c31f7ec273959386025a55331ad87f3c
ronit <ronit@sambuq.com>
2025-01-21 23:21:56 +0530

    app.js updated

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.post('/api/tenders', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
